7a61e0de5d99352c7f9ead22078f4ceee6daf07b
Stefano Pagnottelli <stefano.pagnottelli@siralab.com>
2025-12-17 21:11:18 +0100

    addon build files moved in a custom repository

--- docker-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-DELAY=${MESHMESH_RESTART_DELAY:-5}   # default to 5 seconds if not set
-
-while true; do
-  ./meshmeshgo --config /data/options.json
-  echo "meshmeshgo crashed (exit code $?). Restarting in ${DELAY}s..."
-  sleep "$DELAY"
-done
