5391f0d9d0f7b91cd207365ecc03873baee5545a
Stefano Pagnottelli <stefano.pagnottelli@siralab.com>
2025-12-22 17:03:58 +0100

    updated UI with logo

--- web/src/dashboard/logocard.tsx
+++ web/src/dashboard/logocard.tsx
@@ -8,7 +8,7 @@ export const LogoCard = () => {
                 <Typography component="h2" variant="subtitle2" gutterBottom>Application revision</Typography>
                 <Stack direction="row" sx={{ justifyContent: 'space-between', alignItems: 'center' }}>
                     <Typography variant="body1" component="div">MeshMeshGo</Typography>
-                    <img src="/logo.png" alt="Logo" width="64px" height="64px" />
+                    <img src="logo.png" alt="Logo" width="64px" height="64px" />
                 </Stack>
             </CardContent>
         </Card>
